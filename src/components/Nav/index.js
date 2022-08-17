21ba8e47be6cedc91b3c386a3dee84de7c3cc209
NicolasNegrier <nicolas.negrier@gmail.com>
2022-08-17 13:29:48 +0200

    modification de la nav barre avec les icones

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,14 +2,29 @@
 import { NavLink } from 'react-router-dom';
 import './styles.scss';
 
+// == Import Icones
+import { FaHome } from '@react-icons/all-files/fa/FaHome';
+import { RiCalendarEventFill } from '@react-icons/all-files/ri/RiCalendarEventFill';
+import { GoChecklist } from '@react-icons/all-files/go/GoChecklist';
+
 // == Composant
 const Nav = () => {
 	return (
 		<div className="nav">
-			<NavLink to="/todolists" >ToDoList</NavLink>
-      <NavLink to="/home" >Home</NavLink>
-      <NavLink to="/events" >Events</NavLink>
-		</div>
+
+			<NavLink to="/todolists" >
+        <RiCalendarEventFill className='nav__icon' />
+      </NavLink>
+
+      <NavLink to="/home" >
+        <FaHome className='nav__icon'/>
+      </NavLink>
+
+      <NavLink to="/events" >
+        <GoChecklist className='nav__icon'/>
+      </NavLink>
+		
+    </div>
 	);
 };
 
